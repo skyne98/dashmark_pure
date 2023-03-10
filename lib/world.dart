d90c069411b6db147ec99d5ac0ed0d51fbf77a80
skyne98 <ahalahan@gmail.com>
2023-03-10 21:23:31 +0100

    Drawing the dash!

--- lib/world.dart
+++ lib/world.dart
@@ -1,12 +1,23 @@
 import 'dart:math';
 import 'dart:ui';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/widgets.dart' show debugPrint;
 
 class World {
   var _turn = 0.0;
   double _x;
   double _y;
+  Image? dashImage;
 
-  World(this._x, this._y);
+  World(this._x, this._y) {
+    rootBundle.load('assets/images/dash.png').then((data) {
+      final asUInt8List = data.buffer.asUint8List();
+      decodeImageFromList(asUInt8List, (result) {
+        dashImage = result;
+        debugPrint('Loaded image ${dashImage!.width}x${dashImage!.height}');
+      });
+    });
+  }
 
   void input(double x, double y) {
     _x = x;
@@ -16,14 +27,19 @@ class World {
   void render(double t, Canvas canvas) {
     var tau = pi * 2;
 
-    canvas.drawPaint(Paint()..color = Color(0xff880000));
+    canvas.drawPaint(Paint()..color = const Color(0xff880000));
     canvas.save();
     canvas.translate(_x, _y);
     canvas.rotate(tau * _turn);
-    var white = Paint()..color = Color(0xffffffff);
+    var white = Paint()..color = const Color(0xffffffff);
     var size = 200.0;
     canvas.drawRect(Rect.fromLTWH(-size / 2, -size / 2, size, size), white);
     canvas.restore();
+
+    // Draw the dash image
+    if (dashImage != null) {
+      canvas.drawImage(dashImage!, const Offset(0, 0), Paint());
+    }
   }
 
   void update(double t) {
