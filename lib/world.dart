c882768392d789e3338c10d0300bd37b9e5d848d
skyne98 <ahalahan@gmail.com>
2023-03-13 17:57:01 +0100

    Add a smaller dash

--- lib/world.dart
+++ lib/world.dart
@@ -12,7 +12,7 @@ class World {
   double lastDt = 0.0;
 
   Image? dashImage;
-  double desiredSize = 100.0;
+  double desiredSize = 64.0;
   double scaleToSize = 0.0;
 
   Vector2 _spawnPosition = Vector2(0.0, 0.0);
@@ -33,7 +33,7 @@ class World {
 
   World() {
     debugPrint('World created');
-    rootBundle.load('assets/images/dash.png').then((data) {
+    rootBundle.load('assets/images/dash_128.png').then((data) {
       final asUInt8List = Uint8List.view(data.buffer);
       decodeImageFromList(asUInt8List).then((result) {
         dashImage = result;
